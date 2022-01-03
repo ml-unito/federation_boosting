be748ddc559d2add19d1477cb78bd28c78b29b33
Mirko <mak1788@gmail.com>
2022-01-03 16:32:03 +0100

    Setup experiments
    - Added noniid.py with functions to distribute the examples in a non-iid manner
    - Added ijcnn_exps that is a clean version of fed_multi_adaboost.py to run the "official" experiments
    - Added a bunch of datasets to the data folder

---
+++ ijcnn_exps.py
@@ -0,0 +1,434 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, annotations
+from os import name
+from typing import Any, Tuple, Dict, List, Generator, Optional
+from copy import deepcopy
+from math import log
+import pandas as pd
+import numpy as np
+from numpy.random import choice
+from sklearn.base import ClassifierMixin
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_svmlight_file
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from optparse import OptionParser
+import json
+import wandb
+import noniid
+
+from fed_adaboost import Boosting, split_dataset
+
+####################### ATTENTION #######################
+# Set WANDB to True if you want to use Weights & biases #
+#########################################################
+WANDB = False                                           
+#########################################################
+RANDOM_SEEDS = [98765, 12345, 999999, 101010, 765432, 171717, 13579, 24680]
+
+def manage_options() -> Dict[str, Any]:
+    parser = OptionParser(usage="usage: %prog [options] dataset",
+                          version="%prog 0.1",
+                          description="Testing Samme, Distboost and Preweak adaptation of Samme (Cooper et al. 2017) "\
+                          "as well as Adaboost.F1 (Polato, Esposito, et al. 2022) for multi-class classification. "\
+                          "Supported dataset: adult, letter, forestcover, splice, vehicle, vowel, "\
+                          "segmentation, kr-vs-kp, sat and pendigits")
+    parser.add_option("-s", "--seed",
+                      dest="seed", default=0, type="int", 
+                      help="Pseudo-random seed for replicability purposes - default=98765. Seeds from 0 to 7 are automatically"\
+                           "mapped to 'better' seeds.")
+    parser.add_option("-t", "--test_size",
+                      dest="test_size", default=.2, type="float",
+                      help="Test set size in percentage (0,1) - default=0.2")
+    parser.add_option("-n", "--nclients",
+                      dest="n_clients", default=10, type="int",
+                      help="Number of clients (>= 1) - default=10")
+    parser.add_option("-m", "--model",
+                      dest="model", default="adaboost", type="str",
+                      help="The model to train and test. Supported models: samme, distsamme, "\
+                           "preweaksamme and adaboost.f1 - default=samme")
+    parser.add_option("-z", "--normalize",
+                      dest="normalize", default=False, action="store_true",
+                      help="Whether the instances has to be normalized or not - default=False")
+    parser.add_option("-d", "--noniid",
+                      dest="non_iidness", default=False, type="int",
+                      help="Whether the instances have to be distributed in a non-iid way - default=0.\n" \
+                            "0 - uniform distribution (iid); \n" \
+                            "1 - examples' quantity skewness;\n" \
+                            "2 - labels skewness;\n" \
+                            "3 - Dirichlet distributed labels skewness;\n" \
+                            "4 - pathological labels skewness; \n" \
+                            "5 - covariate shift.")
+    parser.add_option("-l", "--labels",
+                      dest="tags", default="", type="str",
+                      help="list of comma separated tags to be added in the wandb lod - default=''")
+    (options, args) = parser.parse_args()
+    if len(args) < 1:
+        print("Dataset argument missing. Please check the documentation: `python fed_multi_adaboost.py -h`")
+        exit(0)
+    options.dataset = args[0]
+    return options
+
+
+def load_classification_dataset(name: str,
+                                test_size: float=0.2, #real range (0,1)
+                                seed: int=98765) -> Tuple[np.ndarray, np.ndarray]:
+    if name == "letter":
+        df = pd.read_csv("data/letter.csv", header=None)
+        feats = ["feat_%d" %i for i in range(df.shape[1]-1)]
+        df.columns = ["label"] + feats
+        X = df[feats].to_numpy()
+        y = LabelEncoder().fit_transform(df["label"].to_numpy())
+    elif name == "pendigits":
+        df_tr = pd.read_csv("data/pendigits.tr.csv", header=None)
+        df_te = pd.read_csv("data/pendigits.te.csv", header=None)
+        y_tr = df_tr.loc[:, 16].to_numpy()
+        y_te = df_te.loc[:, 16].to_numpy()
+        X_tr = df_tr.loc[:, :15].to_numpy()
+        X_te = df_te.loc[:, :15].to_numpy()
+        return X_tr, X_te, y_tr, y_te
+    elif name == "sat":
+        df_tr = pd.read_csv("data/sat.tr.csv", sep=" ", header=None)
+        df_te = pd.read_csv("data/sat.te.csv", sep=" ", header=None)
+        y_tr = df_tr.loc[:, 36].to_numpy()
+        y_te = df_te.loc[:, 36].to_numpy()
+        X_tr = df_tr.loc[:, :35].to_numpy()
+        X_te = df_te.loc[:, :35].to_numpy()
+        le = LabelEncoder().fit(y_tr)
+        y_tr = le.transform(y_tr)
+        y_te = le.transform(y_te)
+        return X_tr, X_te, y_tr, y_te
+    elif name in {"segmentation", "adult"}:
+        X_tr, y_tr = load_svmlight_file("data/" + name + ".tr.svmlight")
+        X_te, y_te = load_svmlight_file("data/" + name + ".te.svmlight")
+        X_tr = X_tr.toarray()
+        X_te = X_te.toarray()
+        return X_tr, X_te, y_tr, y_te
+    else:
+        X, y = load_svmlight_file("data/" + name + ".svmlight")
+        X = X.toarray()
+        y = y.astype("int")
+
+    return train_test_split(X, y, test_size=test_size, random_state=seed)
+
+
+class MulticlassBoosting(Boosting):
+    def __init__(self: MulticlassBoosting,
+                 n_clf: int=10,
+                 clf_class: ClassifierMixin=DecisionTreeClassifier()):
+        super(MulticlassBoosting, self).__init__(n_clf, clf_class)
+        self.K = None #to be defined in the fit method
+
+    def predict(self: Boosting,
+                X: np.ndarray) -> np.ndarray:
+        y_pred = np.zeros((np.shape(X)[0], self.K))
+        for i, clf in enumerate(self.clfs):
+            pred = clf.predict(X)
+            for j, c in enumerate(pred):
+                y_pred[j, c] += self.alpha[i]
+        return np.argmax(y_pred, axis=1)
+
+
+class Samme(MulticlassBoosting):
+    def fit(self: Samme,
+            X: np.ndarray,
+            y: np.ndarray,
+            checkpoints: Optional[List[int]]=None,
+            seed: int=42) -> Generator[Samme]:
+
+        np.random.seed(seed)
+        self.K = len(set(y)) # assuming that all classes are in y
+        cks = set(checkpoints) if checkpoints is not None else [self.n_clf]
+        n_samples = X.shape[0]
+        D = np.full(n_samples, (1 / n_samples))
+        self.clfs = []
+        self.alpha = []
+        for t in range(self.n_clf):
+            clf = deepcopy(self.clf_class)
+            ids = choice(n_samples, size=n_samples, replace=True, p=D)
+            X_, y_ = X[ids], y[ids]
+            clf.fit(X_, y_)
+
+            predictions = clf.predict(X)
+            min_error = np.sum(D[y != predictions]) / np.sum(D)
+            self.alpha.append(log((1.0 - min_error) / (min_error + 1e-10)) + log(self.K-1)) # kind of additive smoothing
+            D *= np.exp(self.alpha[t] * (y != predictions))
+            D /= np.sum(D)
+            self.clfs.append(clf)
+
+            if (t+1) in cks:
+                yield self
+
+
+# Support class for Distboost
+class Hyp():
+    def __init__(self: Hyp,
+                 ht: List[ClassifierMixin],
+                 K: int):
+        self.ht = ht
+        self.K = K
+    
+    def predict(self: Hyp,
+                X: np.ndarray) -> np.ndarray:
+        y_pred = np.zeros((X.shape[0], self.K))
+        for h in self.ht:
+            pred = h.predict(X)
+            for j, c in enumerate(pred):
+                y_pred[j, int(c)] += 1
+        return np.argmax(y_pred, axis=1)
+
+
+class DistSamme(MulticlassBoosting):
+    def fit(self: DistSamme,
+            X: np.ndarray,
+            y: np.ndarray,
+            checkpoints: Optional[List[int]]=None,
+            seed: int=42) -> Generator[DistSamme]:
+
+        np.random.seed(seed)
+        self.K = len(set(np.concatenate(y))) # assuming that all classes are in y
+        cks = set(checkpoints) if checkpoints is not None else [self.n_clf]
+        n_samples = sum([x.shape[0] for x in X])
+        D = [np.full(x.shape[0], (1 / n_samples)) for x in X]
+        self.clfs = []
+        self.alpha = []
+        for t in range(self.n_clf):
+            ht = []
+            for j, X_ in enumerate(X):
+                clf = deepcopy(self.clf_class)
+                ids = choice(X_.shape[0], size=X_.shape[0], replace=True, p=D[j]/np.sum(D[j]))
+                X__, y__ = X_[ids], y[j][ids]
+                clf.fit(X__, y__)
+                ht.append(clf)
+
+            H = Hyp(ht, self.K)
+            self.clfs.append(H)
+            
+            min_error = 0
+            predictions = []
+            for j, X_ in enumerate(X):
+                predictions.append(H.predict(X_))
+                min_error += np.sum(D[j][y[j] != predictions[j]]) #/ np.sum(D[j])
+            self.alpha.append(log((1.0 - min_error) / (min_error + 1e-10)) + log(self.K-1)) # kind of additive smoothing
+
+            for j, X_ in enumerate(X):
+                D[j] *= np.exp(self.alpha[t] * (y[j] != predictions[j]))
+            Dsum = sum([np.sum(d) for d in D])
+            
+            for d in D:
+                d /= Dsum
+
+            if (t+1) in cks:
+                yield self
+
+
+class PreweakSamme(MulticlassBoosting):
+    def fit(self: PreweakSamme,
+            X: np.ndarray,
+            y: np.ndarray,
+            checkpoints: Optional[List[int]]=None,
+            seed: int=42) -> Generator[PreweakSamme]:
+        
+        np.random.seed(seed)
+        self.K = len(set(np.concatenate(y))) # assuming that all classes are in y
+        cks = set(checkpoints) if checkpoints is not None else [self.n_clf]
+        ht = []
+        for j, X_ in enumerate(X):
+            clf = Samme(self.n_clf, self.clf_class)
+            for h in clf.fit(X_, y[j], checkpoints):
+                pass
+            ht.extend(clf.clfs)
+
+        # merge the datasets into one (not possible in a real distributed/federated scenario)
+        X_ = np.vstack(X) 
+        y_ = np.concatenate(y)
+        
+        # precompute the predictions so then I simply have to draw according to the sampling 
+        ht_pred = {h : h.predict(X_) for h in ht}
+        n_samples = X_.shape[0]
+        D = np.full(n_samples, (1 / n_samples))
+        self.clfs = []
+        self.alpha = []
+        for t in range(self.n_clf):
+            ids = choice(X_.shape[0], size=X_.shape[0], replace=True, p=D)
+            y__ = y_[ids]
+
+            min_error = 1000
+            top_model = None
+            for h, hpred in ht_pred.items():
+                err = np.sum(D[y__ != hpred[ids]]) #/ np.sum(D)
+                if err < min_error:
+                    top_model = h
+                    min_error = err
+
+            self.alpha.append(log((1.0 - min_error) / (min_error + 1e-10)) + log(self.K-1)) # kind of additive smoothing
+            D *= np.exp(self.alpha[t] * (y_ != ht_pred[top_model]))
+            D /= np.sum(D)
+            self.clfs.append(top_model)
+
+            if (t+1) in cks:
+                yield self
+
+
+class AdaboostF1(MulticlassBoosting):
+
+    def federated_dist(self: AdaboostF1,
+                       D: np.ndarray,
+                       X: List[np.ndarray],
+                       j: int) -> np.ndarray:
+        min_index = sum([len(X[i]) for i in range(j)])
+        D_ = D[min_index : min_index + X[j].shape[0]]
+        return D_ / sum(D_)
+
+
+    def fit(self: AdaboostF1,
+            X: List[np.ndarray],
+            y: List[np.ndarray],
+            checkpoints: Optional[List[int]]=None,
+            seed: int=42) -> Generator[AdaboostF1]:
+
+        np.random.seed(seed)
+        self.K = len(set(np.concatenate(y))) # assuming that all classes are in y
+        cks = set(checkpoints) if checkpoints is not None else [self.n_clf]
+
+        # merge the datasets into one (not possible in a real distributed/federated scenario)
+        X_ = np.vstack(X) 
+        y_ = np.concatenate(y)
+
+        n_samples = X_.shape[0]
+        D = np.full(n_samples, (1 / n_samples))
+
+        self.clfs = []
+        self.alpha = []
+
+        for t in range(self.n_clf):
+            fed_clfs = []
+            for j, X__ in enumerate(X):
+                D_ = self.federated_dist(D, X, j)
+                n_samples_ = X__.shape[0]
+
+                clf = deepcopy(self.clf_class)
+                ids = choice(n_samples_, size=n_samples_, replace=True, p=D_)
+                clf.fit(X__[ids], y[j][ids])
+                fed_clfs.append(clf)
+
+            errors = np.array([sum(D[y_ != clf.predict(X_)]) for clf in fed_clfs])
+            best_clf = fed_clfs[np.argmin(errors)]
+            best_error = errors[np.argmin(errors)]
+
+            predictions = best_clf.predict(X_)
+
+            self.alpha.append(log((1.0 - best_error) / (best_error + 1e-10)) + log(self.K-1)) # kind of additive smoothing
+            D *= np.exp(self.alpha[t] * (y_ != predictions))
+
+            D /= np.sum(D)
+            self.clfs.append(best_clf)
+
+            if (t+1) in cks:
+                yield self
+
+
+def distribute_dataset(X, y, n, non_iidness):
+    if non_iidness == 0:
+        return split_dataset(X, y, n)
+    elif non_iidness == 1:
+        ass = noniid.quantity_skew(X, y, n)
+    elif non_iidness == 2:
+        ass = noniid.quantity_skew_lbl(X, y, n, class_per_client=2)
+    elif non_iidness == 3:
+        ass = noniid.dist_skew_lbl(X, y, n, beta=.5)
+    elif non_iidness == 4:
+        ass = noniid.pathological_dist_skew_lbl(X, y, n, shards_per_client=2)
+    elif non_iidness == 5:
+        ass = noniid.covariate_shift(X, y, n, modes=2)
+    else:
+        raise ValueError("Unknown non_iidness code %d!" %non_iidness)
+
+    X_tr = [X[a] for a in ass]
+    y_tr = [y[a] for a in ass]
+    return X_tr, y_tr
+
+
+if __name__ == "__main__":
+    
+    MODEL_NAMES: List[str] = ["samme", "distsamme", "preweaksamme", "adaboost.f1"]
+    options: Dict[str, Any] = manage_options()
+    print("Configuration:\n", json.dumps(vars(options), indent=4, sort_keys=True))
+    assert options.model in MODEL_NAMES, "Model %s not supported!" %options.model
+    
+    MODEL: str = options.model
+    DATASET: str = options.dataset
+    TAGS: List[str] = options.tags.split(",")
+    if WANDB:
+        wandb.init(project='FederatedAdaboost',
+                   entity='mlgroup',
+                   name="%s_%s" %(MODEL, DATASET),
+                   tags=[DATASET, MODEL] + TAGS,
+                   config=options)
+    
+    TEST_SIZE: float = options.test_size
+    NORMALIZE: bool = options.normalize
+    N_CLIENTS: int = options.n_clients
+    SEED: int = RANDOM_SEEDS[options.seed] if options.seed < 8 else options.seed
+    NON_IIDNESS = options.non_iidness
+
+    WEAK_LEARNER = DecisionTreeClassifier(random_state=SEED, max_leaf_nodes=10)
+    N_ESTIMATORS: List[int] = [1] + list(range(10, 301, 10))
+    METRICS: List[str] = ["accuracy", "precision", "recall", "f1"]
+
+    X_train, X_test, y_train, y_test = load_classification_dataset(name=DATASET,
+                                                                   test_size=TEST_SIZE,
+                                                                   seed=SEED)
+    if NORMALIZE:
+        scaler = StandardScaler().fit(X_train)   
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+
+    print("# weak learners: %s" %N_ESTIMATORS)
+    print("Training set size: %d" %X_train.shape[0])
+    print("Test set size: %d" %X_test.shape[0])
+    X_tr, y_tr = distribute_dataset(X_train, y_train, N_CLIENTS, NON_IIDNESS)
+
+    if MODEL == "samme": 
+        model = Samme(max(N_ESTIMATORS), WEAK_LEARNER)
+        X_, y_ = X_train, y_train
+    elif MODEL == "distsamme":
+        model = DistSamme(max(N_ESTIMATORS), WEAK_LEARNER)
+        X_, y_ = X_tr, y_tr
+    elif MODEL == "preweaksamme":
+        model = PreweakSamme(max(N_ESTIMATORS), WEAK_LEARNER)
+        X_, y_ = X_tr, y_tr
+    elif MODEL == "adaboost.f1":
+        model = AdaboostF1(max(N_ESTIMATORS), WEAK_LEARNER)
+        X_, y_ = X_tr, y_tr
+    else:
+        raise ValueError("Unknown model %s." %MODEL)
+
+    print("Training...")
+    for strong_learner in model.fit(X_, y_, N_ESTIMATORS):
+        y_pred_tr = strong_learner.predict(X_train)
+        y_pred_te = strong_learner.predict(X_test)
+        step = strong_learner.num_weak_learners()
+
+        log_dict = {
+            "train" : {
+                "n_estimators" : step,
+                "accuracy": accuracy_score(y_train, y_pred_tr), 
+                "precision": precision_score(y_train, y_pred_tr, average="micro"),
+                "recall": recall_score(y_train, y_pred_tr, average="micro"),
+                "f1": f1_score(y_train, y_pred_tr, average="micro")
+            },
+            "test" : {
+                "n_estimators" : step,
+                "accuracy": accuracy_score(y_test, y_pred_te), 
+                "precision": precision_score(y_test, y_pred_te, average="micro"),
+                "recall": recall_score(y_test, y_pred_te, average="micro"),
+                "f1": f1_score(y_test, y_pred_te, average="micro")
+            }
+        }
+
+        if WANDB: wandb.log(log_dict, step=step)
+        else: print(log_dict)
+
+    print("Training complete!")
---
+++ noniid.py
@@ -0,0 +1,254 @@
+from typing import List, Tuple
+import numpy as np
+from numpy.random import randint, shuffle, power, choice, dirichlet, normal, permutation
+from sklearn.decomposition import PCA
+from scipy.stats.mstats import mquantiles
+
+def quantity_skew(X: np.ndarray,
+                  y: np.ndarray,
+                  n: int,
+                  min_quantity: int=2,
+                  alpha: float=4.) -> List[np.ndarray]:
+    """
+    Distribute the examples across the users according to the following probability density function:
+    $P(x; a) = a x^{a-1}$
+    where x is the id of a client (x in [0, n-1]), and a = `alpha` > 0 with
+    - alpha = 1  => examples are equidistributed across clients;
+    - alpha = 2  => the examples are "linearly" distributed across users;
+    - alpha >= 3 => the examples are power law distributed;
+    - alpha -> \infty => all users but one have `min_quantity` examples, and the remaining user all the rest.
+    Each client is guaranteed to have at least `min_quantity` examples.
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The examples.
+    y: np.ndarray
+        The labels.
+    n: int
+        The number of clients upon which the examples are distributed.
+    min_quantity: int, default 2
+        The minimum quantity of examples to assign to each user.
+    alpha: float=4.
+        Hyper-parameter of the power law density function  $P(x; a) = a x^{a-1}$.
+
+    Returns
+    -------
+    n-dimensional list of arrays. The examples' ids assignment.
+    """
+    assert min_quantity*n <= X.shape[0], "# of instances must be > than min_quantity*n"
+    assert min_quantity > 0, "min_quantity must be >= 1"
+    s = np.array(power(alpha, X.shape[0] - min_quantity*n) * n, dtype=int)
+    m = np.array([[i] * min_quantity for i in range(n)]).flatten()
+    assignment = np.concatenate([s, m])
+    shuffle(assignment)
+    return [np.where(assignment == i)[0] for i in range(n)]
+
+
+def quantity_skew_lbl(X: np.ndarray,
+                      y: np.ndarray,
+                      n: int,
+                      class_per_client: int=2) -> List[np.ndarray]:
+    """
+    Suppose each party only has data samples of `class_per_client` (i.e., k) different labels.
+    We first randomly assign k different label IDs to each party. Then, for the samples of each
+    label, we randomly and equally divide them into the parties which own the label.
+    In this way, the number of labels in each party is fixed, and there is no overlap between
+    the samples of different parties.
+    See: https://arxiv.org/pdf/2102.02079.pdf
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The examples.
+    y: np.ndarray
+        The lables.
+    n: int
+        The number of clients upon which the examples are distributed.
+    class_per_client: int, default 2
+        The number of different labels in each client.
+
+    Returns
+    -------
+    n-dimensional list of arrays. The examples' ids assignment.
+    """
+    labels = set(y)
+    assert 0 < class_per_client <= len(labels), "class_per_client must be > 0 and <= #classes"
+    assert class_per_client * n >= len(labels), "class_per_client * n must be >= #classes"
+    nlbl = [choice(len(labels), class_per_client, replace=False)  for u in range(n)]
+    check = set().union(*[set(a) for a in nlbl])
+    while len(check) < len(labels):
+        missing = labels - check
+        for m in missing:
+            nlbl[randint(0, n)][randint(0, class_per_client)] = m
+        check = set().union(*[set(a) for a in nlbl])
+    class_map = {c:[u for u, lbl in enumerate(nlbl) if c in lbl] for c in labels}
+    assignment = np.zeros(y.shape[0])
+    for lbl, users in class_map.items():
+        ids = np.where(y == lbl)[0]
+        assignment[ids] = choice(users, len(ids))
+    return [np.where(assignment == i)[0] for i in range(n)]
+
+
+def noise_feat_dist_skew(X: np.ndarray,
+                         y: np.ndarray,
+                         n: int,
+                         sigma: float=0.001) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    The function divide the whole dataset into multiple parties randomly and equally.
+    For each party, we add different levels of Gaussian noise to its local dataset to
+    achieve different feature distributions. Specifically, given user-defined noise level sigma,
+    we add noises ∼ Gau(sigma * i/N) for Party i, where Gau(sigma * i/N) is a Gaussian distribution
+    with mean 0 and variance sigma * i/N.
+    See: https://arxiv.org/pdf/2102.02079.pdf
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The examples.
+    y: np.ndarray
+        The lables.
+    n: int
+        The number of clients upon which the examples are distributed.
+    sigma: float, default 0.001
+        The noise level to apply to the features.
+
+    Returns
+    -------
+    Tuple with two lists
+      1. list of arrays representing the examples of each user;
+      2. list of arrays representing the labels of each user.
+    """
+    assignment = choice(n, X.shape[0], replace=True)
+    X_i, y_i = [], []
+    for i in range(n):
+        ids = np.where(assignment == i)[0]
+        X_i.append(X[ids] + normal(0, sigma * (i+1)/n, (len(ids), X.shape[1])))
+        y_i.append(y[ids])
+    return X_i, y_i
+
+
+def dist_skew_lbl(X: np.ndarray,
+                  y: np.ndarray,
+                  n: int,
+                  beta: float=.5) -> List[np.ndarray]:
+    """
+    The function samples p_k ~ Dir_n (beta) and allocate a p_{k,j} proportion of the instances of
+    class k to party j. Here Dir(_) denotes the Dirichlet distribution and beta is a
+    concentration parameter (beta > 0).
+    See: https://arxiv.org/pdf/2102.02079.pdf
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The examples.
+    y: np.ndarray
+        The lables.
+    n: int
+        The number of clients upon which the examples are distributed.
+    beta: float, default .5
+        The beta parameter of the Dirichlet distribution, i.e., Dir(beta).
+
+    Returns
+    -------
+    n-dimensional list of arrays. The examples' ids assignment.
+    """
+    assert beta > 0, "beta must be > 0"
+    labels = set(y)
+    pk = {c: dirichlet([beta]*n, size=1)[0] for c in labels}
+    assignment = np.zeros(y.shape[0])
+    for c in labels:
+        ids = np.where(y == c)[0]
+        assignment[ids] = choice(n, size=len(ids), p=pk[c])
+    return [np.where(assignment == i)[0] for i in range(n)]
+
+
+def pathological_dist_skew_lbl(X: np.ndarray,
+                               y: np.ndarray,
+                               n: int,
+                               shards_per_client: int=2):
+    """
+    The function first sort the data by label, divide it into `n * shards_per_client` shards, and
+    assign each of n clients `shards_per_client` shards. This is a pathological non-IID partition
+    of the data, as most clients will only have examples of a limited number of classes.
+    See: http://proceedings.mlr.press/v54/mcmahan17a/mcmahan17a.pdf
+
+    Parameters
+    ----------
+    X: np.ndarray
+        The examples.
+    y: np.ndarray
+        The lables.
+    n: int
+        The number of clients upon which the examples are distributed.
+    shards_per_client: int, default 2
+        Number of shards per client.
+
+    Returns
+    -------
+    n-dimensional list of arrays. The examples' ids assignment.
+    """
+    sorted_ids = np.argsort(y)
+    n_shards = int(shards_per_client * n)
+    shard_size = int(np.ceil(len(y) / n_shards))
+    assignments = np.zeros(y.shape[0])
+    perm = permutation(n_shards)
+    j = 0
+    for i in range(n):
+        for _ in range(shards_per_client):
+            left = perm[j] * shard_size
+            right = min((perm[j]+1) * shard_size, len(y))
+            assignments[sorted_ids[left:right]] = i
+            j += 1
+    return [np.where(assignments == i)[0] for i in range(n)]
+
+
+def covariate_shift(X: np.ndarray,
+                    y: np.ndarray,
+                    n: int,
+                    modes: int=2):
+    """
+    The function first extracts the first principal component (through PCA) and then divides it in
+    `modes` percentiles. To each user, only examples from a single mode are selected (uniformly).
+    
+    Parameters
+    ----------
+    X: np.ndarray
+        The examples.
+    y: np.ndarray
+        The lables.
+    n: int
+        The number of clients upon which the examples are distributed.
+    modes: int, default 2
+        Number of different modes to consider in the input data first principal component.
+    
+    Returns
+    -------
+    n-dimensional list of arrays. The examples' ids assignment.
+    """
+    assert 2 <= modes <= n, "modes must be >= 2 and <= n"
+
+    ids_mode = [[] for _ in range(modes)]
+    for lbl in set(y):
+        ids = np.where(y == lbl)[0]
+        X_pca = PCA(n_components=2).fit_transform(X[ids])
+        quantiles = mquantiles(X_pca[:, 0], prob=np.linspace(0, 1, num=modes+1)[1:-1])
+
+        y_ = np.zeros(y[ids].shape)
+        for i, q in enumerate(quantiles):
+            if i == 0: continue
+            id_pos = np.where((quantiles[i-1] < X_pca[:, 0]) & (X_pca[:, 0] <= quantiles[i]))[0]
+            y_[id_pos] = i
+        y_[np.where(X_pca[:, 0] > quantiles[-1])[0]] = modes-1
+
+        for m in range(modes):
+            ids_mode[m].extend(ids[np.where(y_ == m)[0]])
+
+    ass_mode = (list(range(modes)) * int(np.ceil(n/modes)))[:n]
+    shuffle(ass_mode)
+    mode_map = {m:[u for u, mu in enumerate(ass_mode) if mu == m] for m in range(modes)}
+    assignment = np.zeros(y.shape[0])
+    for mode, users in mode_map.items():
+        ids = ids_mode[mode]
+        assignment[ids] = choice(users, len(ids))
+    return [np.where(assignment == i)[0] for i in range(n)]
