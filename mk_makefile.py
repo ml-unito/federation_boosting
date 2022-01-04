ddd2b784f985295121917ac705589eb8e38b7a63
Roberto Esposito <roberto.esposito@unito.it>
2022-01-04 20:37:09 +0100

    Cleaned up code and output of mk_makefile.py

--- mk_makefile.py
+++ mk_makefile.py
@@ -1,22 +1,24 @@
 import itertools
 
-
-# Python command line: python ijcnn_exps.py --seed=0 --nclients=10 --model=adaboost.f1 --noniid=3 --labels=IJCNN pendigits
-
 EXPS = "adult,letter,forestcover,splice,vehicle,vowel,segmentation,kr-vs-kp,sat,pendigits".split(",")
 SEEDS = [0, 1, 2, 3]
 MODELS = "samme,distsamme,preweaksamme,adaboost.f1".split(",")
-NONIID= [0, 1, 2, 3, 4, 5]
+NONIID = [0, 1, 2, 3, 4, 5]
 
-experiments = list(itertools.product(EXPS, SEEDS, MODELS, NONIID))
+# Generates a Makefile for the launching the experiments.
+# Usage:
+#   mk_makefile.py > Makefile
 
-experiment_tags = []
-for experiment in experiments:
-    ds, seed, model, noniid = experiment
-    experiment_tags.append("ijcnnexps_ds_{}_model_{}_noniid_{}_seed_{}".format(ds, model, noniid, seed))
-    print("{}:".format(experiment_tags[-1]))
-    print("\techo python ijcnn_exps.py --seed={} --nclients=10 --model={} --noniid={} --labels=IJCNN {}".format(seed, model, noniid, ds))
-    print("\tsleep 1")
+if __name__ == '__main__':
+    experiments = list(itertools.product(EXPS, SEEDS, MODELS, NONIID))
 
-print("all:{}".format(" ".join(experiment_tags)))
+    experiment_tags = []
+    for experiment in experiments:
+        ds, seed, model, noniid = experiment
+        experiment_tags.append("ijcnnexps_ds_{}_model_{}_noniid_{}_seed_{}".format(ds, model, noniid, seed))
+        print("{}:".format(experiment_tags[-1]))
+        print("\t@echo python ijcnn_exps.py --seed={} --nclients=10 --model={} --noniid={} --labels=IJCNN {}".format(seed, model, noniid, ds))
+        print("\t@sleep 1")
+
+    print("all:{}".format(" ".join(experiment_tags)))
 
