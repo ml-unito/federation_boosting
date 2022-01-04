55b8081386b1e1d34fa88460359a0ed0e11d4b00
Roberto Esposito <roberto.esposito@unito.it>
2022-01-04 20:33:24 +0100

    Improved implementation

--- mk_makefile.py
+++ mk_makefile.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 # Python command line: python ijcnn_exps.py --seed=0 --nclients=10 --model=adaboost.f1 --noniid=3 --labels=IJCNN pendigits
 
 EXPS = "adult,letter,forestcover,splice,vehicle,vowel,segmentation,kr-vs-kp,sat,pendigits".split(",")
@@ -5,15 +8,15 @@ SEEDS = [0, 1, 2, 3]
 MODELS = "samme,distsamme,preweaksamme,adaboost.f1".split(",")
 NONIID= [0, 1, 2, 3, 4, 5]
 
+experiments = list(itertools.product(EXPS, SEEDS, MODELS, NONIID))
+
 experiment_tags = []
-for exp in EXPS:
-    for seed in SEEDS:
-        for model in MODELS:
-            for noniid in NONIID:
-                experiment_tags.append("{}_{}_{}_{}".format(exp, seed, model, noniid))
-                print("{}:".format(experiment_tags[-1]))
-                print("\techo python ijcnn_exps.py --seed={} --nclients=10 --model={} --noniid={} --labels=IJCNN {}".format(seed, model, noniid, exp, exp))
-                print("\tsleep 1")
+for experiment in experiments:
+    ds, seed, model, noniid = experiment
+    experiment_tags.append("ijcnnexps_ds_{}_model_{}_noniid_{}_seed_{}".format(ds, model, noniid, seed))
+    print("{}:".format(experiment_tags[-1]))
+    print("\techo python ijcnn_exps.py --seed={} --nclients=10 --model={} --noniid={} --labels=IJCNN {}".format(seed, model, noniid, ds))
+    print("\tsleep 1")
 
 print("all:{}".format(" ".join(experiment_tags)))
 
