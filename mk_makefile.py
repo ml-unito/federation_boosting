87cf569a15bc35e9f2bd22fc3c7d8ad8e0a40d94
Roberto Esposito <roberto.esposito@unito.it>
2022-01-10 17:04:17 +0100

    Add stats command to mk_makefile

--- mk_makefile.py
+++ mk_makefile.py
@@ -12,7 +12,7 @@ SEEDS = [0, 1, 2, 3]
 MODELS = "samme,distsamme,preweaksamme,adaboost.f1".split(",")
 NONIID = "uniform,num_examples_skw,lbl_skw,dirichlet_lbl_skw,pathological_skw,covariate_shift".split(",")
 
-def experiment_to_skip(ds, seed, model, noniid, verbose):
+def experiment_to_skip(ds:str, seed:int, model:str, noniid:str, verbose:bool):
     """
     Returns True if the experiment should be skipped.
     Presently: datasets "adult" e "kr-vs-kp" should be skipped when iidness is in 
@@ -33,25 +33,44 @@ def experiment_to_skip(ds, seed, model, noniid, verbose):
         return True
 
     return False
-    
+
+def experiment_list(verbose:bool=False):
+    experiments = list(itertools.product(EXPS, SEEDS, MODELS, NONIID))
+    result = []
+
+    for experiment in experiments:
+        ds, seed, model, noniid = experiment
+
+        if experiment_to_skip(ds, seed, model, noniid, verbose):
+            continue
+
+        result.append(experiment)
+
+    return result
+
 
 @app.command()
-def main(outfile:str=typer.Argument("Makefile"), verbose:bool=False, test_run:bool=True):
+def stats(verbose:bool=False):
+    """
+    Prints statistics about the experiments to be generated.
+    """
+    experiments = experiment_list(verbose)
+    console.log(f"[bold yellow]Stats[/]: # experiments: {len(experiments)}")
+
+@app.command()
+def run(outfile:str=typer.Argument("Makefile"), verbose:bool=False, test_run:bool=True):
     """
     Generates a Makefile allowing to launch the experiments presented in  (Polato, Esposito, et al. 2022)
     """
 
     test_run_opt = "--test-run" if test_run else "--no-test-run"
 
-    experiments = list(itertools.product(EXPS, SEEDS, MODELS, NONIID))
+    experiments = experiment_list()
 
     with open(outfile, "w") as f:
         experiment_tags = []
         for experiment in experiments:
             ds, seed, model, noniid = experiment
-
-            if experiment_to_skip(ds, seed, model, noniid, verbose):
-                continue
 
             experiment_tags.append(f"logs/ijcnnexps_ds_{ds}_model_{model}_noniid_{noniid}_seed_{seed}.log")
             print(f"{experiment_tags[-1]}:", file=f)
@@ -65,4 +84,4 @@ def main(outfile:str=typer.Argument("Makefile"), verbose:bool=False, test_run:bo
 
 
 if __name__ == '__main__':
-    typer.run(main)
+    app()
