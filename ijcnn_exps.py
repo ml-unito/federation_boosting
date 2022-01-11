1d9c61ae62cc29afd640c1a866b91683bb08a494
Roberto Esposito <roberto.esposito@unito.it>
2022-01-11 17:38:50 +0100

    Refactoring and improved experiment manager

--- ijcnn_exps.py
+++ ijcnn_exps.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, annotations
-from typing import Tuple, List, Generator, Optional
+from typing import Tuple, List
 from copy import deepcopy
-from math import log, ceil
+from math import ceil
 import pandas as pd
 import numpy as np
-from numpy.random import choice
-from sklearn.base import ClassifierMixin
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.datasets import load_svmlight_file
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from fed_adaboost import split_dataset
+from pathlib import Path
+
 
 import rich.traceback as traceback
 from rich.console import Console
 
-import wandb
 import noniid
+from fed_algorithms import *
+
+import wandb
 import typer
 from enum import Enum
+
 
 # Handlers
 
@@ -27,8 +31,6 @@ app = typer.Typer()
 console = Console(record=True)
 error_console = Console(stderr=True)
 traceback.install(show_locals=False)
-
-from fed_adaboost import Boosting, split_dataset
 
 # Enum type for samme, distsamme,preweaksamme and adaboost.f1
 class FedAlgorithms(Enum):
@@ -38,6 +40,8 @@ class FedAlgorithms(Enum):
     adaboost = "adaboost.f1"
 
 # Enum type for datasets: adult, letter, forestcover, splice, vehicle, vowel, segmentation, kr-vs-kp, sat and pendigits
+
+
 class Datasets(Enum):
     adult = "adult"
     letter = "letter"
@@ -57,6 +61,8 @@ class Datasets(Enum):
 #    3 - Dirichlet distributed labels skewness
 #    4 - pathological labels skewness
 #    5 - covariate shift.
+
+
 class Noniidness(Enum):
     uniform = "uniform"
     num_examples_skw = "num_examples_skw"
@@ -67,11 +73,11 @@ class Noniidness(Enum):
 
 
 def load_classification_dataset(name: str,
-                                test_size: float=0.2, #real range (0,1)
-                                seed: int=98765) -> Tuple[np.ndarray, np.ndarray]:
+                                test_size: float = 0.2,  # real range (0,1)
+                                seed: int = 98765) -> Tuple[np.ndarray, np.ndarray]:
     if name == "letter":
         df = pd.read_csv("data/letter.csv", header=None)
-        feats = ["feat_%d" %i for i in range(df.shape[1]-1)]
+        feats = ["feat_%d" % i for i in range(df.shape[1]-1)]
         df.columns = ["label"] + feats
         X = df[feats].to_numpy()
         y = LabelEncoder().fit_transform(df["label"].to_numpy())
@@ -107,222 +113,6 @@ def load_classification_dataset(name: str,
         y = LabelEncoder().fit_transform(y)
 
     return train_test_split(X, y, test_size=test_size, random_state=seed)
-
-
-class MulticlassBoosting(Boosting):
-    def __init__(self: MulticlassBoosting,
-                 n_clf: int=10,
-                 clf_class: ClassifierMixin=DecisionTreeClassifier()):
-        super(MulticlassBoosting, self).__init__(n_clf, clf_class)
-        self.K = None #to be defined in the fit method
-
-    def predict(self: Boosting,
-                X: np.ndarray) -> np.ndarray:
-        y_pred = np.zeros((np.shape(X)[0], self.K))
-        for i, clf in enumerate(self.clfs):
-            pred = clf.predict(X)
-            for j, c in enumerate(pred):
-                y_pred[j, int(c)] += self.alpha[i]
-        return np.argmax(y_pred, axis=1)
-
-
-class Samme(MulticlassBoosting):
-    def fit(self: Samme,
-            X: np.ndarray,
-            y: np.ndarray,
-            checkpoints: Optional[List[int]]=None,
-            seed: int=42) -> Generator[Samme]:
-
-        np.random.seed(seed)
-        self.K = len(set(y)) # assuming that all classes are in y
-        cks = set(checkpoints) if checkpoints is not None else [self.n_clf]
-        n_samples = X.shape[0]
-        D = np.full(n_samples, (1 / n_samples))
-        self.clfs = []
-        self.alpha = []
-        for t in range(self.n_clf):
-            clf = deepcopy(self.clf_class)
-            ids = choice(n_samples, size=n_samples, replace=True, p=D)
-            X_, y_ = X[ids], y[ids]
-            clf.fit(X_, y_)
-
-            predictions = clf.predict(X)
-            min_error = np.sum(D[y != predictions]) / np.sum(D)
-            self.alpha.append(log((1.0 - min_error) / (min_error + 1e-10)) + log(self.K-1)) # kind of additive smoothing
-            D *= np.exp(self.alpha[t] * (y != predictions))
-            D /= np.sum(D)
-            self.clfs.append(clf)
-
-            if (t+1) in cks:
-                yield self
-
-
-# Support class for Distboost
-class Hyp():
-    def __init__(self: Hyp,
-                 ht: List[ClassifierMixin],
-                 K: int):
-        self.ht = ht
-        self.K = K
-    
-    def predict(self: Hyp,
-                X: np.ndarray) -> np.ndarray:
-        y_pred = np.zeros((X.shape[0], self.K))
-        for h in self.ht:
-            pred = h.predict(X)
-            for j, c in enumerate(pred):
-                y_pred[j, int(c)] += 1
-        return np.argmax(y_pred, axis=1)
-
-
-class DistSamme(MulticlassBoosting):
-    def fit(self: DistSamme,
-            X: np.ndarray,
-            y: np.ndarray,
-            checkpoints: Optional[List[int]]=None,
-            seed: int=42) -> Generator[DistSamme]:
-
-        np.random.seed(seed)
-        self.K = len(set(np.concatenate(y))) # assuming that all classes are in y
-        cks = set(checkpoints) if checkpoints is not None else [self.n_clf]
-        n_samples = sum([x.shape[0] for x in X])
-        D = [np.full(x.shape[0], (1 / n_samples)) for x in X]
-        self.clfs = []
-        self.alpha = []
-        for t in range(self.n_clf):
-            ht = []
-            for j, X_ in enumerate(X):
-                clf = deepcopy(self.clf_class)
-                ids = choice(X_.shape[0], size=X_.shape[0], replace=True, p=D[j]/np.sum(D[j]))
-                X__, y__ = X_[ids], y[j][ids]
-                clf.fit(X__, y__)
-                ht.append(clf)
-
-            H = Hyp(ht, self.K)
-            self.clfs.append(H)
-            
-            min_error = 0
-            predictions = []
-            for j, X_ in enumerate(X):
-                predictions.append(H.predict(X_))
-                min_error += np.sum(D[j][y[j] != predictions[j]]) #/ np.sum(D[j])
-            self.alpha.append(log((1.0 - min_error) / (min_error + 1e-10)) + log(self.K-1)) # kind of additive smoothing
-
-            for j, X_ in enumerate(X):
-                D[j] *= np.exp(self.alpha[t] * (y[j] != predictions[j]))
-            Dsum = sum([np.sum(d) for d in D])
-            
-            for d in D:
-                d /= Dsum
-
-            if (t+1) in cks:
-                yield self
-
-
-class PreweakSamme(MulticlassBoosting):
-    def fit(self: PreweakSamme,
-            X: np.ndarray,
-            y: np.ndarray,
-            checkpoints: Optional[List[int]]=None,
-            seed: int=42) -> Generator[PreweakSamme]:
-        
-        np.random.seed(seed)
-        self.K = len(set(np.concatenate(y))) # assuming that all classes are in y
-        cks = set(checkpoints) if checkpoints is not None else [self.n_clf]
-        ht = []
-        for j, X_ in enumerate(X):
-            clf = Samme(self.n_clf, self.clf_class)
-            for h in clf.fit(X_, y[j], checkpoints):
-                pass
-            ht.extend(clf.clfs)
-
-        # merge the datasets into one (not possible in a real distributed/federated scenario)
-        X_ = np.vstack(X) 
-        y_ = np.concatenate(y)
-        
-        # precompute the predictions so then I simply have to draw according to the sampling 
-        ht_pred = {h : h.predict(X_) for h in ht}
-        n_samples = X_.shape[0]
-        D = np.full(n_samples, (1 / n_samples))
-        self.clfs = []
-        self.alpha = []
-        for t in range(self.n_clf):
-            ids = choice(X_.shape[0], size=X_.shape[0], replace=True, p=D)
-            y__ = y_[ids]
-
-            min_error = 1000
-            top_model = None
-            for h, hpred in ht_pred.items():
-                err = np.sum(D[y__ != hpred[ids]]) #/ np.sum(D)
-                if err < min_error:
-                    top_model = h
-                    min_error = err
-
-            self.alpha.append(log((1.0 - min_error) / (min_error + 1e-10)) + log(self.K-1)) # kind of additive smoothing
-            D *= np.exp(self.alpha[t] * (y_ != ht_pred[top_model]))
-            D /= np.sum(D)
-            self.clfs.append(top_model)
-
-            if (t+1) in cks:
-                yield self
-
-
-class AdaboostF1(MulticlassBoosting):
-
-    def federated_dist(self: AdaboostF1,
-                       D: np.ndarray,
-                       X: List[np.ndarray],
-                       j: int) -> np.ndarray:
-        min_index = sum([len(X[i]) for i in range(j)])
-        D_ = D[min_index : min_index + X[j].shape[0]]
-        return D_ / sum(D_)
-
-
-    def fit(self: AdaboostF1,
-            X: List[np.ndarray],
-            y: List[np.ndarray],
-            checkpoints: Optional[List[int]]=None,
-            seed: int=42) -> Generator[AdaboostF1]:
-
-        np.random.seed(seed)
-        self.K = len(set(np.concatenate(y))) # assuming that all classes are in y
-        cks = set(checkpoints) if checkpoints is not None else [self.n_clf]
-
-        # merge the datasets into one (not possible in a real distributed/federated scenario)
-        X_ = np.vstack(X) 
-        y_ = np.concatenate(y)
-
-        n_samples = X_.shape[0]
-        D = np.full(n_samples, (1 / n_samples))
-
-        self.clfs = []
-        self.alpha = []
-
-        for t in range(self.n_clf):
-            fed_clfs = []
-            for j, X__ in enumerate(X):
-                D_ = self.federated_dist(D, X, j)
-                n_samples_ = X__.shape[0]
-
-                clf = deepcopy(self.clf_class)
-                ids = choice(n_samples_, size=n_samples_, replace=True, p=D_)
-                clf.fit(X__[ids], y[j][ids])
-                fed_clfs.append(clf)
-
-            errors = np.array([sum(D[y_ != clf.predict(X_)]) for clf in fed_clfs])
-            best_clf = fed_clfs[np.argmin(errors)]
-            best_error = errors[np.argmin(errors)]
-
-            predictions = best_clf.predict(X_)
-
-            self.alpha.append(log((1.0 - best_error) / (best_error + 1e-10)) + log(self.K-1)) # kind of additive smoothing
-            D *= np.exp(self.alpha[t] * (y_ != predictions))
-
-            D /= np.sum(D)
-            self.clfs.append(best_clf)
-
-            if (t+1) in cks:
-                yield self
 
 
 def distribute_dataset(X, y, n, non_iidness, seed):
@@ -456,16 +246,23 @@ def run(dataset: Datasets = typer.Argument(...),
     as well as Adaboost.F1 (Polato, Esposito, et al. 2022) for multi-class classification. "
     """
 
-    filename = f"ijcnnexps_ds_{dataset.value}_model_{model.value}_noniid_{non_iidness.value}_seed_{seed}"
+    filename = f"logs/ijcnnexps_ds_{dataset.value}_model_{model.value}_noniid_{non_iidness.value}_seed_{seed}"
+    run_file = filename+".run"
+    err_file = filename+".err"
+    log_file = filename+".log"
+
+    Path(run_file).touch()
 
     try:
         execute_experiment(dataset, seed, test_size, n_clients, model, normalize, non_iidness, tags, test_run)
 
         console.log("Training complete!")
-        console.save_text("logs/" + filename + ".log")
+        console.save_text(log_file)
     except:
         console.print_exception(show_locals=True)
-        console.save_text("logs/" + filename + ".err")
+        console.save_text(err_file)
+
+    Path(run_file).unlink()
 
 if __name__ == "__main__":
     typer.run(run)
